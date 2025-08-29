0edbc9dc4a17037ebf57e3afd028be855a4298ee
Siddh <siddh@example.com>
2025-08-29 21:04:18 +0530

    basic ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";   
+import NavBar from "./NavBar";
+import Body from "./Body";
+import Login from "./Login";
+import Profile from "./Profile";
+
 
 
 function App() {
  
 
-  return (
+  return ( 
     <>
-      
-      <h1 className="font-bold bg-black text-white">hello world</h1>
+    <BrowserRouter basename="/">
+    <Routes>
+      <Route path="/" element ={<Body />} >
+         <Route path="/login" element ={<Login />} />
+         <Route path="/Profile" element ={<Profile />} />
+      </Route>
+     </Routes>
+    </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return(
+        <footer className="footer bg-base-200 text-neutral text-neutral-content p-4 fixed bottom-0">
+  <aside>
+    <p>Copyright © {new Date().getFullYear()} - All right reserved by ACME Industries Ltd</p>
+  </aside>
+</footer>
+    );
+};
+export default Footer;
